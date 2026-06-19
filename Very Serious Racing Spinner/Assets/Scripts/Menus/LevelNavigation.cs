277106ed32a6ce407eaa781fcb0dd77c8f539917
Arturo <arturo.kojiki@gmail.com>
2026-06-19 19:17:44 -0400

    Implemented Preliminary Failed Level Menu and Test Navigation

--- Very Serious Racing Spinner/Assets/Scripts/Menus/LevelNavigation.cs
+++ Very Serious Racing Spinner/Assets/Scripts/Menus/LevelNavigation.cs
@@ -9,16 +9,29 @@ public class LevelNavigation : MonoBehaviour
         SceneManager.LoadScene("LevelCompleteMenu");
     }
 
+
+    public void NavigateToLevelFailedScene()
+    {
+        SceneManager.LoadScene("LevelFailedMenu");
+    }
+
+
     // Level navigation function (called whenever the player completes a level).
     public void NextLevel()
     {
-        Debug.Log("TEST");
         StaticData.currentLevel++;
         string nextLevelName = "Level" + StaticData.currentLevel.ToString();
-        Debug.Log(nextLevelName);
         SceneManager.LoadScene(nextLevelName);
     }
 
+    // Level failed function (called whenever the player fails a level).
+    public void RestartLevel()
+    {
+        string currentLevelName = "Level" + StaticData.currentLevel.ToString();
+        SceneManager.LoadScene(currentLevelName);
+    }
+
+    // TODO: save the player's data (position, stats, and so forth).
     public void PauseLevel()
     {
         StaticData.pausedLevelName = SceneManager.GetActiveScene().name;
