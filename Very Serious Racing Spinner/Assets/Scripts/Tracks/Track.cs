0fcc2db77dcf9f463b2087dad2b7c71a9c562c95
Arturo <arturo.kojiki@gmail.com>
2026-06-23 23:44:02 -0400

    Very Rough Direction Fields (Requires Modifications for the Actual Car)

--- Very Serious Racing Spinner/Assets/Scripts/Tracks/Track.cs
+++ Very Serious Racing Spinner/Assets/Scripts/Tracks/Track.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Track : MonoBehaviour
 {
-    [SerializeField] float directionFieldForce = 2.5f;
+    [SerializeField] float ring1Force = 2.5f;
+    [SerializeField] float ring2Force = 5f;
+    [SerializeField] float ring3Force = 5f;
+    [SerializeField] float ring4Force = -10f;
 
     private void OnTriggerStay(Collider other)
     {
@@ -11,10 +14,27 @@ public class Track : MonoBehaviour
             Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
 
             // Direction force field.
-            var direction = transform.right;
-            rb.AddForce(directionFieldForce * direction);
-
-            Debug.Log(directionFieldForce * direction);
+            if (gameObject.tag.Equals("Ring1"))
+            {
+                var direction = transform.right;
+                rb.AddForce(ring1Force * direction);
+            }
+            if (gameObject.tag.Equals("Ring2"))
+            {
+                var direction = transform.right;
+                rb.AddForce(ring2Force * direction);
+                Debug.Log(ring2Force * direction);
+            }
+            if (gameObject.tag.Equals("Ring3"))
+            {
+                var direction = transform.right;
+                rb.AddForce(ring3Force * direction);
+            }
+            if (gameObject.tag.Equals("Ring4"))
+            {
+                var direction = transform.right;
+                rb.AddForce(ring4Force * direction);
+            }
         }
     }
 }
