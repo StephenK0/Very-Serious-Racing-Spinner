0ed6629ce0ac44052c30aa51020351872ff4c944
Arturo <arturo.kojiki@gmail.com>
2026-06-25 10:13:30 -0400

    Temporary Save (Tangential Force)

--- Very Serious Racing Spinner/Assets/Scripts/Tracks/Track.cs
+++ Very Serious Racing Spinner/Assets/Scripts/Tracks/Track.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Track : MonoBehaviour
 {
@@ -7,34 +8,58 @@ public class Track : MonoBehaviour
     [SerializeField] float ring3Force = 5f;
     [SerializeField] float ring4Force = -10f;
 
+    public float DotProduct(Vector3 a, Vector3 b)
+    {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+
+    public float Length(Vector3 a)
+    {
+        return Mathf.Sqrt(DotProduct(a, a));
+    }
+    
+
+    public float GetAngle(Vector3 a, Vector3 b)
+    {
+        return Mathf.Acos(DotProduct(a, b) / (Length(a) * Length(b)));
+    }
+
+
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag.Equals("Player"))
         {
             Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
 
+            // Obtain the angle between the Player and the vertical plane/line.
+            Vector3 verticalVector = new Vector3(0, 0, 5);
+            Vector3 playerToCenter = rb.position;
+
+            float angle = GetAngle(verticalVector, playerToCenter);
+            Vector3 direction = new Vector3(-Mathf.Sin(angle), 0, Mathf.Cos(angle));
+            Debug.Log("ANGLE:" + angle);
+            rb.AddForce(direction);
+
             // Direction force field.
             if (gameObject.tag.Equals("Ring1"))
             {
-                var direction = transform.right;
-                rb.AddForce(ring1Force * direction);
+                //rb.AddForce(ring1Force * direction);
+                //Debug.Log(direction);
+                //Debug.Log("POSITION:" + rb.position);
             }
-            if (gameObject.tag.Equals("Ring2"))
-            {
-                var direction = transform.right;
-                rb.AddForce(ring2Force * direction);
-                Debug.Log(ring2Force * direction);
-            }
-            if (gameObject.tag.Equals("Ring3"))
-            {
-                var direction = transform.right;
-                rb.AddForce(ring3Force * direction);
-            }
-            if (gameObject.tag.Equals("Ring4"))
-            {
-                var direction = transform.right;
-                rb.AddForce(ring4Force * direction);
-            }
+            //if (gameObject.tag.Equals("Ring2"))
+            //{
+            //    rb.AddForce(ring2Force * direction * -1);
+            //}
+            //if (gameObject.tag.Equals("Ring3"))
+            //{
+            //    rb.AddForce(ring3Force * direction);
+            //}
+            //if (gameObject.tag.Equals("Ring4"))
+            //{
+            //    rb.AddForce(ring4Force * direction * -1);
+            //}
         }
     }
 }
