0c5db591e6012bd5a574954834911e56d631ec45
Arturo <arturo.kojiki@gmail.com>
2026-06-26 10:53:27 -0400

    Updated Level1 and Level2 Sizing and Colliders

--- Very Serious Racing Spinner/Assets/Scripts/Tracks/Track.cs
+++ Very Serious Racing Spinner/Assets/Scripts/Tracks/Track.cs
@@ -1,12 +1,19 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Timeline;
 using UnityEngine.UIElements;
 
 public class Track : MonoBehaviour
 {
-    [SerializeField] float ring1Force = 2.5f;
-    [SerializeField] float ring2Force = 5f;
-    [SerializeField] float ring3Force = 5f;
-    [SerializeField] float ring4Force = -10f;
+    [SerializeField] float ring1Speed = 10000000f;
+    [SerializeField] float ring2Speed = -100f;
+    [SerializeField] float ring3Speed = 100f;
+    [SerializeField] float ring4Speed = -150f;
+
+    Vector3 direction = new Vector3(1, 0, 0);
+    public List<GameObject> onTrack;
+
 
     public float DotProduct(Vector3 a, Vector3 b)
     {
@@ -28,38 +35,112 @@ public class Track : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
+        Debug.Log(other.gameObject.tag);
+
         if (other.gameObject.tag.Equals("Player"))
         {
             Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
 
-            // Obtain the angle between the Player and the vertical plane/line.
-            Vector3 verticalVector = new Vector3(0, 0, 5);
+            Vector3 verticalVector = new Vector3(0, 0, 85);
             Vector3 playerToCenter = rb.position;
 
             float angle = GetAngle(verticalVector, playerToCenter);
-            Vector3 direction = new Vector3(-Mathf.Sin(angle), 0, Mathf.Cos(angle));
-            Debug.Log("ANGLE:" + angle);
-            rb.AddForce(direction);
+            direction = new Vector3(-Mathf.Sin(angle), 0, Mathf.Cos(angle));
 
-            // Direction force field.
-            if (gameObject.tag.Equals("Ring1"))
+            Debug.Log("Player...");
+        }
+
+
+        if (other.gameObject.tag.Equals("Ring1"))
+        {
+            for (int i = 0; i < onTrack.Count; i++)
             {
-                //rb.AddForce(ring1Force * direction);
-                //Debug.Log(direction);
-                //Debug.Log("POSITION:" + rb.position);
+                onTrack[i].GetComponent<Rigidbody>().linearVelocity = ring1Speed * direction * Time.deltaTime;
             }
-            //if (gameObject.tag.Equals("Ring2"))
-            //{
-            //    rb.AddForce(ring2Force * direction * -1);
-            //}
-            //if (gameObject.tag.Equals("Ring3"))
-            //{
-            //    rb.AddForce(ring3Force * direction);
-            //}
-            //if (gameObject.tag.Equals("Ring4"))
-            //{
-            //    rb.AddForce(ring4Force * direction * -1);
-            //}
+        }
+
+        else if (gameObject.tag.Equals("Ring2"))
+        {
+            for (int i = 0; i < onTrack.Count; i++)
+            {
+                onTrack[i].GetComponent<Rigidbody>().linearVelocity = ring2Speed * direction * Time.deltaTime;
+            }
+        }
+
+        else if (gameObject.tag.Equals("Ring3"))
+        {
+            for (int i = 0; i < onTrack.Count; i++)
+            {
+                onTrack[i].GetComponent<Rigidbody>().linearVelocity = ring3Speed * direction * Time.deltaTime;
+            }
+        }
+
+        else if (gameObject.tag.Equals("Ring4"))
+        {
+            for (int i = 0; i < onTrack.Count; i++)
+            {
+                onTrack[i].GetComponent<Rigidbody>().linearVelocity = ring4Speed * direction *Time.deltaTime;
+            }
+        }
+
+
+
+
+        // OLD CODE:
+        // Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+
+        // Obtain the angle between the Player and the vertical plane/line.
+        //Vector3 verticalVector = new Vector3(0, 0, 5);
+        //Vector3 playerToCenter = rb.position;
+
+        //float angle = GetAngle(verticalVector, playerToCenter);
+        //Vector3 direction = new Vector3(-Mathf.Sin(angle), 0, Mathf.Cos(angle));
+        //Debug.Log("ANGLE:" + angle);
+        //rb.AddForce(direction);
+
+
+        // 
+        //Vector3 forceVector = Quaternion.AngleAxis(90, playerToCenter) * new Vector3(1, 1, 1);
+
+
+
+        // Direction force field.
+        //if (gameObject.tag.Equals("Ring1"))
+        //{
+        //    Debug.Log("FORCE RING1:" + forceVector * ring1Force);
+        //    rb.AddForce(forceVector * 75);
+        //}
+        //else if (gameObject.tag.Equals("Ring2"))
+        //{
+        //    rb.AddForce(ring2Force * forceVector);
+        //}
+        //else if (gameObject.tag.Equals("Ring3"))
+        //{
+        //    rb.AddForce(ring3Force * forceVector);
+        //}
+        //else if (gameObject.tag.Equals("Ring4"))
+        //{
+        //    rb.AddForce(ring4Force * forceVector);
+        //}
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hello...");
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("TEST!");
+            onTrack.Add(collision.gameObject);
+        }
+    }
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Goodbye...");
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            onTrack.Remove(collision.gameObject);
         }
     }
 }
